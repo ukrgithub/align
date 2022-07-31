8492e8d3af69a2ee6928fe42c534fff86cda0274
Karthick Rajkumar <ukarthickrajkumar@gmail.com>
2022-07-31 08:11:10 +0100

    Auto Make
    Signed-off-by: Karthick Rajkumar <ukarthickrajkumar@gmail.com>

---
+++ src/main.c
@@ -0,0 +1,48 @@
+/**
+ * @file main.c
+ * @author Karthick Rajkumar (ukarthickrajkumar@gmail.com)
+ * @brief  Application Entry point
+ * @version 0.1
+ * @date 2022-07-31
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+/**
+ * @file main.c
+ * @author Karthick Rajkumar (ukarthickrajkumar@gmail.com)
+ * @brief  Application Entry point
+ * @version 0.1
+ * @date 2022-07-31
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+/**
+ * -----------------------------------------------------------
+ * Include
+ * -----------------------------------------------------------
+ */
+#include <stdio.h>
+
+
+/**
+ * -----------------------------------------------------------
+ * Functions
+ * -----------------------------------------------------------
+ */
+/**
+ * @brief Application Entry Point
+ * 
+ * @param argc number of command line arguments
+ * @param argv command line arguments list
+ * @return 0 on success, 1 on error
+ */
+int main (int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
+{
+    printf ("Hello World!\n");
+    return 0;
+}
+
+/** EOF */
